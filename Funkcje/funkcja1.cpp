750b876d1dff2dd58607e3dc43dda3bb953f82c9
JakubBlazk <91121129+JakubBlazk@users.noreply.github.com>
2023-11-29 09:16:53 +0100

    Add files via upload

---
+++ Funkcje/funkcja1.cpp
@@ -0,0 +1,17 @@
+#include <stuio.h>
+using namespace std;
+if(sales<10000){
+    salary=salary*110%;
+}else if (salary>10000&&salary<14999.99)
+{
+    salary=salary*112%
+}else if (salary>15000&&salary<17999.99)
+{
+    salary=salary*114%
+}else if (salary>18000&&salary<21999.99)
+{
+    salary=salary*116%
+}else if (salary>22000)
+{
+    salary=salary*118%
+}
